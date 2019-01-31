4da889ecfcf794945b4911e8ca01640bcca54f87
SudarshanBhandari <47011252+SudarshanBhandari@users.noreply.github.com>
2019-01-31 15:34:20 -0500

    Copyright Date added

--- src/ca/sheridancollege/week1/softwarefundamental/Student1.java
+++ src/ca/sheridancollege/week1/softwarefundamental/Student1.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *31 Jan 2019
  */
 package ca.sheridancollege.week1.softwarefundamental;
 
